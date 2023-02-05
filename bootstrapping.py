5987d28e54949c171af53b2e8f395dcb20363726
Paloma <palomaguenes@gmail.com>
2023-02-05 19:09:11 -0300

    feature de colocar nome do arquivo de entrada

--- bootstrapping.py
+++ bootstrapping.py
@@ -1,11 +1,40 @@
+# Criado por Paloma Guenes #
+
 import numpy as np
 import pandas as pd
 import random
 
-print("\nEste script calcula o intervalo de confiança baseado na técnica bootstrap. \n\n")
+#Apresentacao
+print("\nEste script calcula o intervalo de confiança baseado na técnica Bootstrapping. \n\n")
 
-#lendo o arquivo com dados de exemplo (algo parecido vira do survey)
-df = pd.read_csv('150respostasExemplo.csv')
+#Pegando nome do arquivo de entrada
+input_name = input("Escreva o nome do arquivo de entrada ou aperte aperte enter: ")
+
+#Guardando nome do arquivo de entrada default disponibilizado nesta pasta
+default_name = 'testes/150respostasExemplo.csv'
+
+#Checando se o arquivo digitado existe, caso nao exista o programa é encerrado
+if input_name == "":
+  #Lendo o arquivo com dados de exemplo (algo parecido virá do survey)
+  print("Lendo o arquivo: " + default_name + "\n\n")
+  df = pd.read_csv(default_name)  
+else:
+  if (input_name[len(input_name) - 4:] == '.CSV' or input_name[len(input_name) - 4:] == '.csv'):
+    try:
+      with open(input_name, 'r') as f:
+          print("Lendo o arquivo: " + input_name + "\n\n")
+          df = pd.read_csv(f)
+    except IOError:
+      print('Arquivo não encontrado!\n\n')
+      exit()
+  else:
+    print('O arquivo não é csv!\n\n')
+    exit()
+    
+#checar se a hasIP existe
+if 'hasIP' not in list(df.columns):
+  print('O arquivo não contém a coluna hasIP!\n\n')
+  exit()
 
 print("Através dos dados lidos, foram encontradas ")
 
@@ -29,4 +58,3 @@ confidence_level = 0.95
 #calculo do intervalo de confianca
 confidence_interval = np.percentile(sample_perc,[100*(1-confidence_level)/2,100*(1-(1-confidence_level)/2)]) 
 print("com intervalo de confiança de " + str(confidence_interval) +"\n")
-
